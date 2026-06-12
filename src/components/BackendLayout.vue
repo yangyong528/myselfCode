78e24cee2e3fd987e6f8f21dbf140fc97746509d
yangyong528 <cat2199.cool@163.com>
2026-06-12 11:50:36 +0800

    Update BackendLayout.vue
    菜单2

--- src/components/BackendLayout.vue
+++ src/components/BackendLayout.vue
@@ -3,6 +3,9 @@
     <el-menu>
       <el-menu-item index="1">首页菜单0612c</el-menu-item>
     </el-menu>
+    <el-menu>
+      <el-menu-item index="2">菜单2</el-menu-item>
+    </el-menu>
     <router-view></router-view>
   </div>
 </template>
